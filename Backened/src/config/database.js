a007817b47f1a7ccf1cbe69b2a5405f55a840bfc
harshagrawal-coder <harshagrawal9498@gmail.com>
2026-03-20 10:14:27 +0530

    databse code right

--- Backened/src/config/database.js
+++ Backened/src/config/database.js
@@ -1,9 +1,16 @@
-const mongoose = require("mongoose")
-function connection(){
-    mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
-        console.log("database connected")
+const mongoose = require("mongoose");
+
+function connection() {
+  if (!process.env.MONGO_URI) {
+    console.log("No DB URI found, skipping DB connection");
+    return;
+  }
+
+  mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("database connected");
     })
+    .catch(err => console.log(err));
 }
 
-module.exports = connection
+module.exports = connection;
